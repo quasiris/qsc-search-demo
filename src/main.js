9f8098d682db74e695f478d16d060bd3fa202049
jakub <jakubstankowski@protonmail.com>
2020-07-21 14:17:41 +0200

    QSC-319 add search data picker library

--- src/main.js
+++ src/main.js
@@ -5,9 +5,16 @@ import store from './store'
 import vuetify from './plugins/vuetify';
 import axios from 'axios';
 import globalConfig from "../public/config/config";
-
 Vue.config.productionTip = false;
 axios.defaults.baseURL = globalConfig.api_url;
+/*
+import SearchDataPicker from "@quasiris/search-data-picker/src";
+
+const url = "https://qsc-dev.quasiris.de/api/v1/admin/config/hornbach-de/merge/qsc-search-demo";
+SearchDataPicker.initPicker.fromUrl(url);
+*/
+
+
 
 new Vue({
   router,
