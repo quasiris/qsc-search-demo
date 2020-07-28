738de229e232aa0c679498aba92379e53484248f
jakub <jakubstankowski@protonmail.com>
2020-07-28 13:51:41 +0200

    QSC-325 add category tree view, but without children from endpoint

--- src/views/dashboard/Products.vue
+++ src/views/dashboard/Products.vue
@@ -51,12 +51,12 @@
                                 <v-expansion-panel v-for="(filter, i) in PRODUCTS.facets" :key="i">
                                     <v-expansion-panel-header>{{filter.name}}</v-expansion-panel-header>
                                     <v-expansion-panel-content v-if="filter.id === 'category'">
-                                        <ul>
-                                            <li v-for="(filterValue, i) in filter.values"
-                                                :key="i">
-                                                {{filterValue.value}}
-                                            </li>
-                                        </ul>
+                                        <v-treeview
+                                                open-all
+                                                return-object
+                                                dense
+                                                :items="categoryTreeItems"
+                                        />
                                     </v-expansion-panel-content>
                                     <v-expansion-panel-content v-else>
                                         <FilterCheckbox
@@ -207,9 +207,97 @@
                 set(value) {
                     this.priceSliderValue = value;
                 }
+            },
+            categoryTreeItems() {
+                const facets = {...this.$store.state.products.products.facets[4]};
+
+                const treeViewObject = [];
+                treeViewObject.push({
+                    children: facets['values'].map((val) => {
+                        return {
+                            name: `${val.value} (${val.count})`,
+                            filter: val.filter,
+                        }
+                    })
+                });
+                return treeViewObject;
             }
+
+
         },
         data: () => ({
+            items: [
+                {
+                    id: 1,
+                    name: 'Applications :',
+                    children: [
+                        {id: 2, name: 'Calendar : app',},
+                        {id: 3, name: 'Chrome : app'},
+                        {id: 4, name: 'Webstorm : app'},
+                    ],
+                },
+                {
+                    id: 5,
+                    name: 'Documents :',
+                    children: [
+                        {
+                            id: 6,
+                            name: 'vuetify :',
+                            children: [
+                                {
+                                    id: 7,
+                                    name: 'src :',
+                                    children: [
+                                        {id: 8, name: 'index : ts'},
+                                        {id: 9, name: 'bootstrap : ts'},
+                                    ],
+                                },
+                            ],
+                        },
+                        {
+                            id: 10,
+                            name: 'material2 :',
+                            children: [
+                                {
+                                    id: 11,
+                                    name: 'src :',
+                                    children: [
+                                        {id: 12, name: 'v-btn : ts'},
+                                        {id: 13, name: 'v-card : ts'},
+                                        {id: 14, name: 'v-window : ts'},
+                                    ],
+                                },
+                            ],
+                        },
+                    ],
+                },
+                {
+                    id: 15,
+                    name: 'Downloads :',
+                    children: [
+                        {id: 16, name: 'October : pdf'},
+                        {id: 17, name: 'November : pdf'},
+                        {id: 18, name: 'Tutorial : html'},
+                    ],
+                },
+                {
+                    id: 19,
+                    name: 'Videos :',
+                    children: [
+                        {
+                            id: 20,
+                            name: 'Tutorials :',
+                            children: [
+                                {id: 21, name: 'Basic layouts : mp4'},
+                                {id: 22, name: 'Advanced techniques : mp4'},
+                                {id: 23, name: 'All about app : dir'},
+                            ],
+                        },
+                        {id: 24, name: 'Intro : mov'},
+                        {id: 25, name: 'Conference introduction : avi'},
+                    ],
+                },
+            ],
             form: Object.assign({}, form),
             formOptions: {
                 valid: false,
