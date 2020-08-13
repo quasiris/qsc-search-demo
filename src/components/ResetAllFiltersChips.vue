b7c3b1b8f0ce4bfaaf845a41b1f43f8339583d0c
jakub <jakubstankowski@protonmail.com>
2020-08-13 12:44:21 +0200

    QSC-341 fixed mobile devices bugs in ResetAllFiltersChips.vue

--- src/components/ResetAllFiltersChips.vue
+++ src/components/ResetAllFiltersChips.vue
@@ -1,11 +1,11 @@
 <template>
     <v-chip
-            class="mt-3"
+            class="mt-3 reset-all-filters-chips"
             color="teal"
             text-color="white"
             @click="resetAllFiltersChips"
     >
-        Reset All Filters1
+        Reset All Filters
         <v-icon right>
             mdi-close
         </v-icon>
@@ -25,5 +25,13 @@
 </script>
 
 <style scoped>
+    .reset-all-filters-chips {
+        margin-left: 15px;
+    }
 
+    @media (max-width: 1000px) {
+        .reset-all-filters-chips {
+            margin: 0;
+        }
+    }
 </style>
